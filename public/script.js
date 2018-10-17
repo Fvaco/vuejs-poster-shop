9075af1ce818aa893a63f362a4fc7f86b635f6c4
Iván Hernández Ortega <ivan@wupplier.com>
2018-10-17 20:50:59 +0100

    🚀 Working version w/ imgur.com API integration

--- public/script.js
+++ public/script.js
@@ -1,1 +1,64 @@
-console.log('It works.');
+const PRICE = 9.99;
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      total: 0,
+      items: [],
+      cart: [],
+      search: "",
+      lastSearch: '',
+      searching: false
+    };
+  },
+  filters: {
+    currency(value) {
+      return `$${value.toFixed(2)}`;
+    }
+  },
+  methods: {
+    addItem(index) {
+      this.total += PRICE;
+      let { id, title } = this.items[index];
+      let foundIndex = this.cart.findIndex(itemCart => itemCart.id === id);
+
+      if (foundIndex > -1) return this.cart[foundIndex].qty++;
+
+      this.cart.push({
+        id,
+        title,
+        qty: 1,
+        price: PRICE
+      });
+    },
+    increment(item) {
+      item.qty++;
+      this.total += PRICE;
+    },
+    decrement(item) {
+      item.qty--;
+      if (item.qty === 0) {
+        this.cart.splice(
+          this.cart.findIndex(element => element.id === item.id),
+          1
+        );
+      }
+    },
+    onSubmit() {
+        
+        this.items = [];
+        if(this.search.length){
+            this.searching=true;
+            this.$http
+            .get(`/search/${this.search}`)
+            .then(({ data }) => {
+              console.log(data);
+              this.items = data;
+              this.lastSearch = this.search;
+              this.searching=false;
+            });
+        }
+      
+    }
+  }
+});
